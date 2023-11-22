dacaa5011c425444b365d9bf85142eea0f561f9e
Maheshvema <babumahesh2088@gmail.com>
2023-11-22 16:09:03 +0530

    modified

---
+++ DevicesAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevicesAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Demonstrate usage
+            SmartPhone smartphone = new SmartPhone("MyPhone");
+            smartphone.Connect();
+            smartphone.Charge(60);
+            Console.WriteLine(smartphone.Display());
+
+            Console.WriteLine();
+
+            Laptop laptop = new Laptop("MyLaptop");
+            laptop.Connect();
+            laptop.Charge(120);
+            Console.WriteLine(laptop.Display());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DevicesAssignment/Smartphone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevicesAssignment
+{
+    public class SmartPhone : IConnectable, IDisplayable, IRechargeable
+    {
+        public string Model { get; set; }
+
+        public SmartPhone(string model)
+        {
+            Model = model;
+        }
+
+        public bool Connect()
+        {
+            // Implementation for Connect method
+            Console.WriteLine($"{Model} connected successfully.");
+            return true;
+        }
+
+        public void Charge(int minutes)
+        {
+            // Implementation for Charge method
+            Console.WriteLine($"{Model} is charging for {minutes} minutes.");
+        }
+
+        public string Display()
+        {
+            // Implementation for Display method
+            return $"{Model} display: Home screen";
+        }
+    }
+
+
+}
